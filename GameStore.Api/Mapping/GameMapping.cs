6fb46fb26eda1cc819d7a11ea770bb88bcdb0da8
Bhone Wai <damenaosan@gmail.com>
2025-09-02 20:03:14 +0630

    feat: Add data layer, entities and new DTOs

---
+++ GameStore.Api/Mapping/GameMapping.cs
@@ -0,0 +1,53 @@
+using System;
+using GameStore.Api.Dtos;
+using GameStore.Api.Entities;
+
+namespace GameStore.Api.Mapping;
+
+public static class GameMapping
+{
+    public static Game toEntity(this CreateGameDto game)
+    {
+        return new Game()
+        {
+            Name = game.Name,
+            GenreId = game.GenreId,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate,
+        };
+    }
+
+    public static Game toEntity(this UpdateGameDto game, int id)
+    {
+        return new Game()
+        {
+            Id = id,
+            Name = game.Name,
+            GenreId = game.GenreId,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate,
+        };
+    }
+
+    public static GameSummaryDto toGameSummaryDto(this Game game)
+    {
+        return new(
+            game.Id,
+            game.Name,
+            game.Genre!.Name,
+            game.Price,
+            game.ReleaseDate
+        );
+    }
+
+    public static GameDetailsDto toGameDetailsDto(this Game game)
+    {
+        return new(
+            game.Id,
+            game.Name,
+            game.GenreId,
+            game.Price,
+            game.ReleaseDate
+        );
+    }
+}
